dbd33697941c6a324cc86d63363627ce03272064
adhamcodes <219492688+adhamcodes@users.noreply.github.com>
2026-09-01 01:38:13 +0600

    accessibility: load final focus and contrast safeguards

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ import "@/styles/master/index.css";
 import "@/styles/master/travel.css";
 import "@/styles/master/sound.css";
 import "@/styles/master/mobile.css";
+import "@/styles/master/accessibility.css";
 import "@/styles/master/system.css";
 
 const instrumentSans = Instrument_Sans({
