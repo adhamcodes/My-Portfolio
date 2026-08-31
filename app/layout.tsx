7c039e7871a257948a6ad2164d0c8131dc4f0f22
adhamcodes <219492688+adhamcodes@users.noreply.github.com>
2026-09-01 00:33:22 +0600

    prototype(02): load Index experience layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "@/styles/master/tokens.css";
 import "@/styles/master/base.css";
 import "@/styles/master/shell.css";
 import "@/styles/master/trace.css";
+import "@/styles/master/index.css";
 import "@/styles/master/system.css";
 
 export const metadata: Metadata = {
